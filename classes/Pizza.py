4ead9cf9dba32270317d4655c112e52d4e95e2ea
Cristhian Rodriguez <cristhian.j.rodriguez.d.s@gmail.com>
2020-06-17 16:32:23 -0400

    Updates working with SQL / Pizza Class done

--- classes/Pizza.py
+++ classes/Pizza.py
@@ -1,97 +1,71 @@
+from classes.db_model.SQLiteDB import SQLiteDB
+
 class Pizza:
 	"""This class emulates an ordered Pizza"""
 	def __init__(self, size, toppings):
 		"""Initialize size, base toppings and extra toppings"""
 		self.size = size
-		self.extra_toppings_list = ['jamón', 'champiñones', 
-									'pimentón', 'doble queso',
-									'aceitunes', 'pepperoni', 
-									'salchichón']
 		self.toppings = toppings
-		self.toppings.append('salsa de tomate')
-		self.toppings.append('queso')
+		toppings.append('salsa de tomate')
+		toppings.append('queso')
 
-	def pizza_price(self):
-		"""Calculates and returns the total price for the pizza"""
-		price = 0
+	def get_size_price(self):
+		"""
+		From pizza size get id, size, price from db
+		Returns size id, price.
+		"""
+		size_price = 0
+		db = SQLiteDB()
+		connect = db.create_connection()
+		sizes_info = db.get_sizes_rows(connect)
+
 		"""Evaluate pizza size"""
-		if self.size == 'personal':
-			"""Set pizza size price """
-			price += 10
-			for topping in self.toppings:
-				"""Get each topping of the pizza"""
-				if topping in self.extra_toppings_list:
-					"""
-					If topping is an extra search for the extra price 
-					and add it to the total price
-					"""
-					extra_index = self.extra_toppings_list.index(topping)
-					if extra_index == 0:
-						price += 1.5
-					elif extra_index == 1:
-						price += 1.75
-					elif extra_index == 2:
-						price += 1.5
-					elif extra_index == 3:
-						price += 0.8
-					elif extra_index == 4:
-						price += 1.8
-					elif extra_index == 5:
-						price += 1.25
-					elif extra_index == 6:
-						price += 1.6
+		for row in sizes_info:
+			if self.size == row[1]:
+				size_price += row[2]
+				size_id = row[0]
 
-		elif self.size == 'mediana':
-			"""Set pizza size price"""
-			price += 15
-			for topping in self.toppings:
-				"""Get each topping of the pizza"""
-				if topping in self.extra_toppings_list:
-					"""
-					If topping is an extra search for the extra price 
-					and add it to the total price
-					"""
-					extra_index = self.extra_toppings_list.index(topping)
-					if extra_index == 0:
-						price += 1.75
-					elif extra_index == 1:
-						price += 2.05
-					elif extra_index == 2:
-						price += 1.75
-					elif extra_index == 3:
-						price += 1.3
-					elif extra_index == 4:
-						price += 2.15
-					elif extra_index == 5:
-						price += 1.7
-					elif extra_index == 6:
-						price += 1.85
+		"""Return´s the size id, price"""
+		size_price = '{:.2f}'.format(size_price)
+		return size_id, size_price
+		
+	def get_toppings_id(self, size_id):
+		"""Return the topping´s id."""
+		db = SQLiteDB()
+		connect = db.create_connection()
+		toppings_id = db.get_toppings_rows(connect)
+		ids = []
+		
+		"""Evaluate pizza toppings"""
+		for topping in self.toppings:
+			for row in toppings_id:
+				if topping == row[1]:
+					ids.append(row[0])
+				
+		return ids
+		
+		
+	def get_toppings_price(self, toppings_id, size_id):
+		"""
+		From toppings ID calculate the total price of toppings
+		"""
+		topping_price = 0
+		db = SQLiteDB()
+		toppings_info = db.getToppingPricesRows()
+		for id in toppings_id:
+			for row in toppings_info:
+				if size_id == row[1] and id == row[2]:
+					topping_price += row[0]
 
-		elif self.size == 'familiar':
-			price += 20
-			"""Set pizza size price"""
-			for topping in self.toppings:
-				"""Get each topping of the pizza"""
-				if topping in self.extra_toppings_list:
-					"""
-					If topping is an extra search for the extra price 
-					and add it to the total price
-					"""
-					extra_index = self.extra_toppings_list.index(topping)
-					if extra_index == 0:
-						price += 2
-					elif extra_index == 1:
-						price += 2.5
-					elif extra_index == 2:
-						price += 2
-					elif extra_index == 3:
-						price += 1.7
-					elif extra_index == 4:
-						price += 2.6
-					elif extra_index == 5:
-						price += 1.9
-					elif extra_index == 6:
-						price += 2.1
-		"""Resturn´s the final price"""
-		price = '{:.2f}'.format(price)
-		return price
+		"""Return´s the toppings final price"""
+		topping_price = '{:.2f}'.format(topping_price)
+		return topping_price
+	
+	def get_total_price(self, size_price, topping_price):
+		"""Calculates the final price for the pizza"""
+		final_price = size_price + topping_price
+		final_price = '{:.2f}'.format(final_price)
+		return final_price
+		
+
+				
