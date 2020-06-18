2c159a2819614fd5954e1ec966c5e722b0f77027
Andrew <andrescastillo.r@hotmail.com>
2020-06-18 16:06:02 -0400

    fill Order class

--- classes/Order.py
+++ classes/Order.py
@@ -1,2 +1,12 @@
 class Order:
-	pass
+	
+	def __init__(self, name, date, pizzas):
+		self.name = name
+		self.date = date
+		self.pizzas = pizzas
+		
+	def getTotal(self):
+		total = 0
+		for pizza in self.pizzas:
+			total += pizza.get_price_total()
+		return total
